2208f9be610b6c576a783264ed0034afd306b813
grafit.1 <grafit.1@yandex.ru>
2022-02-06 21:29:29 +0300

    feat: add resolution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    const result = [];
+    if (matrix) {
+        for (let i = 0; i < matrix.length; i++) {
+            for (let n = 0; n < matrix[i].length; n++) {
+                let parityCheck = i % 2 === 0 ? n : matrix[i].length - n - 1;
+                result.push(matrix[i][parityCheck]);
+            }
+        }
+    }
+    return result;
+};
